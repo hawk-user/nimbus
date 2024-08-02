aa7c011e15f8590d5257fc771b094825501f227e
Salem <salem.djekoun@gmail.com>
2024-08-02 09:54:16 +0200

    Create a list of ANSI escape codes and add 'RESET' and 'BG_GREEN'

---
+++ src/libs/core/constants/ansi.escape.code.ts
@@ -0,0 +1,22 @@
+
+
+/**
+    * Enum representing ANSI escape codes for text formatting in the terminal.
+    * @readonly
+*/
+
+export enum ANSI_ESCAPE_CODE {
+
+    /**
+        * Resets all formatting to default.
+    */
+
+    RESET = '\u001B[0m',
+
+    /**
+        * Sets the background color to blue.
+    */
+
+    BG_BLUE = '\u001B[44m'
+
+}
