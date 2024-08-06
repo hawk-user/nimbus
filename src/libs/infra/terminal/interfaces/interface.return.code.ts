d117f4dd108757928490cfbdb8b3a30b4725a4d2
Salem <salem.djekoun@gmail.com>
2024-08-06 12:51:32 +0200

    Create a representation of the different codes returned by the program

---
+++ src/libs/infra/terminal/interfaces/interface.return.code.ts
@@ -0,0 +1,40 @@
+
+/**
+    * Representation of the different codes returned by the program.
+*/
+
+export enum ReturnCode {
+
+    /**
+        * Ok.
+        * 
+        * The program has run successfully.
+    */
+
+    OK = 0,
+
+    /**
+        * Idea.
+        * 
+        * The program gives an idea of what might be needed.
+    */
+
+    IDEA = 1,
+
+     /**
+        * Not Found.
+        * 
+        * The program was unable to find the resource.
+    */
+
+    NOT_FOUND = 4,
+
+    /**
+        * Internal.
+        * 
+        * The program encountered an unexpected condition that prevented it from meeting demand.
+    */
+
+    INTERNAL = 5,
+
+}
