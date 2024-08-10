f74c047b98e094185aa7a8ad7eab163cfc9ae611
Salem <salem.djekoun@gmail.com>
2024-08-10 12:44:16 +0200

    Time to move on to the second stage, 'routing'

--- src/infra/terminal/index.ts
+++ src/infra/terminal/index.ts
@@ -1,27 +1,5 @@
 
-import { Program, StandardOutput } from '@ueye/infra';
 import { ConsoleLogger } from '@ueye/core';
 
-const logger = ConsoleLogger.create()
-
-const stdoutImpl: StandardOutput = {
-    display: (stdcode, msg, data) => logger.debug({ stdcode, msg, data })
-};
-
-class HelloWorld extends Program {
-    protected async executeImpl(
-        _stdin: object,
-        stdout: StandardOutput
-    ): Promise<void> {
-        return this.ok(stdout, { msg: 'Hello World!' })
-    }
-
-    static run(
-        stdin: object,
-        stdout: StandardOutput
-    ) {
-        new HelloWorld().execute(stdin, stdout);
-    }
-}
-
-HelloWorld.run({}, stdoutImpl);
+const logger = ConsoleLogger.create();
+logger.info('...Routing in progress');
