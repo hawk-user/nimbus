c832c0161b0766c9f0505075c915dd878bcda169
Salem <salem.djekoun@gmail.com>
2024-08-19 10:56:52 +0200

    Removal of 'interfaces' directory, no longer required

--- libs/infra/terminal/interfaces/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './interface.standard.input';
-export * from './interface.standard.output';
-export * from './interface.standard.error';
--- libs/infra/terminal/interfaces/interface.standard.input.ts
+++
@@ -1,8 +0,0 @@
-
-/**
-    * Interface representing a standard input mechanism.
-*/
-
-export type StandardInput = object;
-
-// export interface StandardInput {}
