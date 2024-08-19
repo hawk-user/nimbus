dd45151ac5a72ffef99fd5a96516b675591a28e9
Salem <salem.djekoun@gmail.com>
2024-08-19 11:03:54 +0200

    Use native NodeJS types for streams

---
+++ libs/infra/terminal/interfaces.stream.ts
@@ -0,0 +1,18 @@
+
+/**
+    * Interface representing a standard error ouput mechanism.
+*/
+
+export type StandardOutput = NodeJS.Process['stdout'];
+
+/**
+    * Interface representing a standard input mechanism.
+*/
+
+export type StandardInput = NodeJS.Process['stdin'];
+
+/**
+    * Interface representing a standard error ouput mechanism.
+*/
+
+export type StandardError = NodeJS.Process['stderr'];
