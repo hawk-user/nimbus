864f59c7344d4503a50c1385302ab1cf81d8bdca
Salem <salem.djekoun@gmail.com>
2024-08-20 14:47:31 +0200

    Implementing a fallback stream

--- libs/infra/terminal/interfaces.stream.ts
+++ libs/infra/terminal/interfaces.stream.ts
@@ -1,18 +1,45 @@
 
 /**
-    * Interface representing a standard error ouput mechanism.
+    * A type combining the properties of `NodeJS.Process['stdout']`
+    * with a fallback stream property `fallback`.
+    * 
+    * @purpose
+    *   Provides a mechanism to define a primary output stream 
+    *   with an alternative stream to be used as a fallback.
 */
 
-export type StandardOutput = NodeJS.Process['stdout'];
+export type FallbackStream = NodeJS.Process['stdout'] & {
+
+    /**
+        * The fallback stream, typically used when the primary 
+        * output stream is unavailable or fails.
+        * 
+        * @caution
+        *   **✋ Read this:** The `fallback` stream should be reserved 
+        *   for use only if the primary `stdout` stream is not accessible 
+        *   or encounters an error.
+    */
+
+    fallback: NodeJS.Process['stderr'];
+
+};
+
 
 /**
-    * Interface representing a standard input mechanism.
+    * Type representing the standard output stream (`stdout`) 
+    * with an fallback mechanism in case of errors.
 */
 
-export type StandardInput = NodeJS.Process['stdin'];
+export type StandardOutput = NodeJS.Process['stdout'] & FallbackStream;
 
 /**
-    * Interface representing a standard error ouput mechanism.
+    * Type representing a standard error ouput mechanism.
 */
 
 export type StandardError = NodeJS.Process['stderr'];
+
+/**
+    * Type representing a standard input mechanism.
+*/
+
+export type StandardInput = NodeJS.Process['stdin'];
