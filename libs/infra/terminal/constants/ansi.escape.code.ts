df74cea7b4ad28d4a3f2c21d2dfa1a17d6a3e6d0
Salem <salem.djekoun@gmail.com>
2024-08-20 16:36:20 +0200

    JSDoc updated with ANSI code additions and deletions

--- libs/infra/terminal/constants/ansi.escape.code.ts
+++ libs/infra/terminal/constants/ansi.escape.code.ts
@@ -1,47 +1,39 @@
 
-
 /**
-    * Enum representing ANSI escape codes for text formatting in the terminal.
-    * @readonly
+    * Enum representing ANSI escape codes for text formatting in terminal outputs.
+    * 
+    * This enum provides a set of constants for various ANSI escape codes used to
+    * control text formatting, such as color and style, in terminal emulators.
 */
 
 export enum AnsiEscapeCodes {
 
-    /**
-        * Resets all formatting to default.
-    */
+    /** Resets the text formatting to the default settings. */
 
     RESET = '\u001B[0m',
 
-    /**
-        * Sets the background color to blue.
-    */
+    /** Sets the background color to cyan. */
 
-    BG_BLUE = '\u001B[44m',
+    BG_CYAN = '\u001B[36m',
 
-    /**
-        * Sets the background color to cyan.
-    */
+    /** Sets the background color to yellow. */
 
-    BG_CYAN = '\u001B[46m',
+    BG_YELLOW = '\u001B[33m',
 
-    /**
-        * Sets the background color to yellow.
-    */
+    /** Sets the background color to red. */
 
-    BG_YELLOW = '\u001B[43m',
+    BG_RED = '\u001B[31m',
 
-    /**
-        * Sets the background color to orange.
-    */
+    /** Sets the background color to green. */
 
-    BG_ORANGE = '\u001B[48;5;214m',
+    BG_GREEN = '\u001B[32m',
 
-    /**
-        * Sets the background color to magenta.
-    */
+    /** Sets the background color to grey. */
+
+    BG_GREY = '\u001B[90m',
+
+    /** Sets the background color to magenta. */
 
     BG_MAGENTA = '\u001B[45m'
-
 
 }
