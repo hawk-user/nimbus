b5e57c86bd2624dd582f2389f7fddfb0541002ef
Salem <salem.djekoun@gmail.com>
2024-08-28 16:43:11 +0200

    Add 'CriticalError' class for formatting unspecified critical errors

---
+++ libs/infra/terminal/formatters/error/critical.error.ts
@@ -0,0 +1,27 @@
+
+import { BaseError } from './base.error';
+import { ExitCodes, CriticalExitCodes } from '../../exit.codes';
+
+/**
+    * A class that provides methods for formatting
+    * critical errors.
+*/
+
+export class CriticalError extends BaseError {
+
+    /**
+        * Formats an unspecified critical error into
+        * a standardized object.
+        *
+        * @param The error object to be formatted.
+        * @returns The formatted error output with the unspecified critical error code.
+    */
+
+    public static unspecifiedError(error: Error) {
+        return this.format<CriticalExitCodes>(
+            error,
+            ExitCodes.UNSPECIFIED_ERROR
+        );
+    }
+
+}
