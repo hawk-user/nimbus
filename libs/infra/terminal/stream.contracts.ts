3a1dd48813993e336c8acd924258c25f555146a3
Salem <salem.djekoun@gmail.com>
2024-09-03 15:38:46 +0200

    Move StreamIdentifier to a more appropriate location

--- libs/infra/terminal/stream.contracts.ts
+++ libs/infra/terminal/stream.contracts.ts
@@ -1,6 +1,4 @@
 
-import { StreamIdentifier } from './stream.identifiers';
-
 /**
     * Interface representing an output stream that can write data.
 */
@@ -102,3 +100,28 @@ export interface IOStream<A>  {
     stdout: StandardOutput
 
 }
+
+/**
+    * Enum representing identifiers for different types of streams.
+    *
+    * - `OUTPUT`: Represents the output stream, associated with `&out`.
+    * - `ERROR`: Represents the error stream, associated with `&err`.
+    * - `INPUT`: Represents the input stream, associated with `&in`.
+*/
+
+export enum StreamIdentifier {
+    OUTPUT = '&out',
+    ERROR = '&err',
+    INPUT = '&in'
+}
+
+/**
+    * Type representing a unique stream identifier.
+    *
+    * Possible values:
+    * - `&out` (Standard ouput)
+    * - `&err` (Standard error)
+    * - `&in` (Standard innput)
+*/
+
+export type UniqueStreamIdentifier = typeof StreamIdentifier[keyof typeof StreamIdentifier];
