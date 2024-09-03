96cd5942eff38ffe3fc2013f1b1fdf250c9345b7
Salem <salem.djekoun@gmail.com>
2024-09-03 13:46:00 +0200

    Export 'StreamActions' from 'terminal' directory

--- libs/infra/terminal/index.ts
+++ libs/infra/terminal/index.ts
@@ -1,5 +1,6 @@
 export * from './exit.codes';
 export * from './formatters';
 export * from './standard.stream';
+export * from './stream.actions';
 export * from './stream.contracts';
 export * from './stream.identifiers';
