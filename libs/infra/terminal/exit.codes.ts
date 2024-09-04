62dfc992d741b1b18a172cd44b1de09b1aef0f27
Salem <salem.djekoun@gmail.com>
2024-09-04 16:29:58 +0200

    Split exit code categories into two groups and make JSDoc more comprehensive

--- libs/infra/terminal/exit.codes.ts
+++ libs/infra/terminal/exit.codes.ts
@@ -1,21 +1,22 @@
 
-// Use of constants to avoid the ESlint no-magic-number
-// error and maintain enumeration consistency
+// Use of constants to avoid the ESlint no-magic-number error
+// and maintain enumeration consistency.
 
 // Success code(s)
 const OK = 0;
 
 // Common error code(s)
 const INTERR = 88;
-
-// Critical error code(s)
 const UNSPEC = 160;
-
-// Custom error code(s)
 const BLKHLED = 255;
 
 /**
     * Enumeration of exit codes.
+    * 
+    * This enum defines a set of standard exit codes used within an application
+    * to indicate the result of operations. It includes a success code, 
+    * a set of common error codes, and also defines the minimum and maximum 
+    * bounds for these codes.
 */
 
 export enum ExitCodes {
@@ -40,6 +41,8 @@ export enum ExitCodes {
 
     /**
         * Blackhole reached (custom exit code).
+        * This is a specific error code representing a non-standard or 
+        * rare condition encountered in the application.
     */
 
     BLACKHOLE_REACHED = BLKHLED,
@@ -60,33 +63,32 @@ export enum ExitCodes {
 
 /**
     * Type representing successful exit codes.
-*/
+    * 
+    * This type is used to represent any exit code that signifies a successful
+    * operation.
+ */
 
 export type SuccessExitCodes = ExitCodes.DONE;
 
 /**
-    * Type representing error exit codes.
+    * Type representing common error exit codes.
+    * 
+    * This type is used to represent a union of exit codes that indicate various
+    * error conditions. It includes:
+    * - `ExitCodes.INTERNAL_ERROR`: An internal error.
+    * - `ExitCodes.UNSPECIFIED_ERROR`: An unspecified error.
+    * - `ExitCodes.BLACKHOLE_REACHED`: A custom error indicating that a blackhole was reached.
 */
 
-export type ErrorExitCodes = ExitCodes.INTERNAL_ERROR;
+export type CommonErrorExitCodes = ExitCodes.INTERNAL_ERROR
+    | ExitCodes.UNSPECIFIED_ERROR
+    | ExitCodes.BLACKHOLE_REACHED;
 
 /**
-    * Type representing critical exit codes.
+    * Type representing any standard exit codes.
+    * 
+    * This type is a union of all possible exit codes that an application
+    * might return, covering both success and error conditions.
 */
 
-export type CriticalExitCodes = ExitCodes.UNSPECIFIED_ERROR;
-
-/**
-    * Type representing custom exit codes.
-*/
-
-export type CustomExitCodes = ExitCodes.BLACKHOLE_REACHED;
-
-/**
-    * Type representing any standard exit code.
-*/
-
-export type StandardExitCodes = SuccessExitCodes
-| ErrorExitCodes
-| CriticalExitCodes
-| CustomExitCodes;
+export type StandardExitCodes = SuccessExitCodes | CommonErrorExitCodes;
