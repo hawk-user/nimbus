d040a15edc086a1be67887094a9f5a9d68bee0cc
Salem <salem.djekoun@gmail.com>
2024-09-04 10:54:45 +0200

    Add utility class for text-related operations

---
+++ libs/utils/text.utils.ts
@@ -0,0 +1,19 @@
+
+/**
+    * Utility class for text-related operations.
+*/
+
+export class TextUtils {
+
+    /**
+        * Checks if a given text string is empty.
+        *
+        * @param text - The text to check.
+        * @returns  Returns true if the text is empty, otherwise false.
+    */
+
+    public static isEmpty(text: string): boolean {
+        return text === '';
+    }
+
+}
