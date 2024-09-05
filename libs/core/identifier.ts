2d455d6d925fdb457c96ab3424f4c58f5dd38bd1
Salem <salem.djekoun@gmail.com>
2024-09-05 21:28:03 +0200

    add abstract Identifier class with comparison logic

---
+++ libs/core/identifier.ts
@@ -0,0 +1,58 @@
+
+import { TextUtils } from '@ueye/utils';
+
+/**
+    * Abstract class representing an Identifier.
+    * 
+    * ***Note:***
+    * 
+    * This class serves as a blueprint for creating specific
+    * types of identifiers.
+    * 
+    * It encapsulates the concept of an identifier with a string
+    * value and provides methods for working with that value.
+    * Subclasses should extend this class to implement specific
+    * types of identifiers.
+*/
+
+export abstract class Identifier {
+
+    /**
+        * The identifier value.
+    */
+
+    private value: string;
+
+    /**
+        * Creates an instance of Identifier.
+        * 
+        * @param {string} value - The identifier value.
+    */
+
+    constructor(value: string) {
+        this.value = value;
+    }
+
+    /**
+        * Returns the identifier value.
+        * @returns The identifier value.
+    */
+
+    protected toValue(): string {
+        return this.value;
+    }
+
+    /**
+        * Compares this identifier to another identifier.
+        * 
+        * @param id - The identifier to compare with.
+        * @returns True if the identifiers are equal, otherwise false.
+    */
+
+    public isEqualTo(id: Identifier): boolean {
+        const isSameClass = id instanceof this.constructor;
+        const haveSameValue = TextUtils.areIdentical(this.value, id.toValue());
+        return isSameClass && haveSameValue;
+    }
+
+}
--- libs/core/index.ts
+++ libs/core/index.ts
@@ -1,3 +1,4 @@
 export * from './gone.wrong';
+export * from './identifier';
 export * from './operation';
 export * from './went.well';
