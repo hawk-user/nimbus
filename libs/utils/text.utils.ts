b14d73d8fe02c3734a04624be92fe2c9e5b74048
Salem <salem.djekoun@gmail.com>
2024-09-05 18:00:00 +0200

    Implements the areIdentical method and adds the JSDoc

--- libs/utils/text.utils.ts
+++ libs/utils/text.utils.ts
@@ -6,6 +6,21 @@
 export class TextUtils {
 
     /**
+        * Compares two strings and returns true if they are exactly the same.
+        *
+        * @param original - The original string to compare.
+        * @param comparison - The string to compare with the original.
+        * @returns Returns true if the strings are the same, otherwise false.
+    */
+
+        public static areIdentical(
+            original: string,
+            comparison: string
+        ): boolean {
+            return original === comparison;
+        }
+
+    /**
         * Checks if a given text string is empty.
         *
         * @param text - The text to check.
