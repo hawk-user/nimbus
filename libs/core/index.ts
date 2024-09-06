a696ceda3d319dcc403c8b2a539fc05297be16d5
Salem <salem.djekoun@gmail.com>
2024-09-06 11:12:59 +0200

    Add Properties class and Structure type to the repository

--- libs/core/index.ts
+++ libs/core/index.ts
@@ -1,5 +1,6 @@
 export * from './gone.wrong';
 export * from './identifier';
 export * from './operation';
+export * from './properties';
 export * from './unique.identifier';
 export * from './went.well';
