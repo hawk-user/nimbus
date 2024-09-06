83eaae4bb5030a26bded3941025f0707db5bdce2
Salem <salem.djekoun@gmail.com>
2024-09-06 14:44:35 +0200

    Add the hasDirectProperty method

--- libs/utils/object.utils.ts
+++ libs/utils/object.utils.ts
@@ -5,14 +5,32 @@
 export class ObjectUtils {
 
     /**
+        * Checks if the specified property exists directly
+        * on the given object.
+        * 
+        * @param target - The object to check for the property.
+        * @param property - The name of the property to check.
+        * 
+        * @returns True if the property exists directly on the
+        * object, otherwise false.
+    */
+
+    public static hasDirectProperty(
+        target: object,
+        property: string
+    ): boolean {
+        return Object.hasOwn(target, property);
+    }
+
+    /**
         * Converts a given object to its JSON string representation.
         *
-        * @param value - The object to convert to a string.
+        * @param target - The object to convert to a string.
         * @returns  The JSON string representation of the object.
     */
 
-    public static toString(value: object): string {
-        return JSON.stringify(value);
+    public static toString(target: object): string {
+        return JSON.stringify(target);
     }
 
 }
