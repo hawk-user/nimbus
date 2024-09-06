761a3fb4c9760007f6ae05c56107f737f4be6b86
Salem <salem.djekoun@gmail.com>
2024-09-06 10:07:39 +0200

    Implements the generateUniqueId method and adds the JSDoc

--- libs/utils/text.utils.ts
+++ libs/utils/text.utils.ts
@@ -13,12 +13,37 @@ export class TextUtils {
         * @returns Returns true if the strings are the same, otherwise false.
     */
 
-        public static areIdentical(
-            original: string,
-            comparison: string
-        ): boolean {
-            return original === comparison;
-        }
+    public static areIdentical(
+        original: string,
+        comparison: string
+    ): boolean {
+        return original === comparison;
+    }
+
+    /**
+        * Generates a unique string identifier.
+        * 
+        * **Note:**
+        * 
+        * This method uses a basic algorithm and should be used only
+        * in small systems where the likelihood of collisions is low.
+        * For larger systems or critical applications, consider using
+        * a more robust identifier generation method.
+        * 
+        * @returns A unique identifier.
+    */
+    
+    public static generateUniqueId(): string {
+        const RADIX = 36;
+        const BEGIN_SUBSTRING = 2;
+        const END_SUBSTRING = 9;
+
+        const timestamp = Date.now().toString(RADIX);
+        const randomPart = Math.random().toString(RADIX)
+            .substring(BEGIN_SUBSTRING, END_SUBSTRING);
+
+        return timestamp + randomPart;
+    }
 
     /**
         * Checks if a given text string is empty.
