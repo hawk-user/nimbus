0f7890f3a22e26c27f78315b496adaca7e171cb0
Salem <salem.djekoun@gmail.com>
2024-09-06 14:42:45 +0200

    Add unit tests for the hasDirectProperty method

--- tests/libs/utils/unit/object.utils.test.ts
+++ tests/libs/utils/unit/object.utils.test.ts
@@ -3,6 +3,41 @@ import { ObjectUtils } from '@ueye/utils';
 
 describe('ObjectUtils specifications', () => {
 
+    describe('hasDirectProperty', () => {
+        
+        it('should return true if the property exists directly on the object', () => {
+            const obj = { a: 1 };
+            expect(ObjectUtils.hasDirectProperty(obj, 'a')).toBe(true);
+        });
+
+        it('should return false if the property does not exist on the object', () => {
+            const obj = { a: 1 };
+            expect(ObjectUtils.hasDirectProperty(obj, 'b')).toBe(false);
+        });
+
+        it('should return false if the property exists on the prototype chain but not directly on the object', () => {
+            const proto = { a: 1 };
+            const obj = Object.create(proto);
+            expect(ObjectUtils.hasDirectProperty(obj, 'a')).toBe(false);
+        });
+
+        it('should return false if the target is an empty object and property does not exist', () => {
+            const obj = {};
+            expect(ObjectUtils.hasDirectProperty(obj, 'a')).toBe(false);
+        });
+
+        it('should handle properties with undefined values', () => {
+            const obj = { a: undefined };
+            expect(ObjectUtils.hasDirectProperty(obj, 'a')).toBe(true);
+        });
+
+        it('should return false if property is an empty string', () => {
+            const obj = { '': 1 };
+            expect(ObjectUtils.hasDirectProperty(obj, '')).toBe(true);
+        });
+
+    });
+
     describe('toString', () => {
 
         it('should return the JSON string representation of an object', () => {
