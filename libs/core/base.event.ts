20386c11d4b1fdd877e121e3e2f42468fb2fcc17
Salem <salem.djekoun@gmail.com>
2024-09-10 17:08:01 +0200

    Add a class to generate generic events

---
+++ libs/core/base.event.ts
@@ -0,0 +1,41 @@
+
+import { Event } from './events';
+import { UniqueIdentifier } from './unique.identifier';
+import { TimePoint } from './time.point';
+
+/**
+    * Represents a generic event.
+    * 
+    * @template K - The type of data associated with the event.
+*/
+
+export class BaseEvent<K>  implements Event<K> {
+
+    uniqueIdentifier: UniqueIdentifier;
+
+    timePoint: TimePoint;
+
+    type: string;
+
+    data: K;
+
+    protected constructor(type: string, data: K) {
+        this.uniqueIdentifier = UniqueIdentifier.create();
+        this.timePoint = TimePoint.create();
+        this.type = type
+        this.data = data;
+    }
+
+    /**
+        * Creates a new `BaseEvent` with the specified data.
+        * 
+        * @template Q - The type of data associated with the event.
+        * @param data - The data that triggered the event.
+        * @returns A new instance of `BaseEvent` with the specified data.
+    */
+
+    public static create<Q>(type: string, data: Q): BaseEvent<Q> {
+        return new BaseEvent(type, data);
+    }
+
+}
