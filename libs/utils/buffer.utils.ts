012b10b2b12e3f4beb6b95055de8c500f169b49c
Salem <salem.djekoun@gmail.com>
2024-09-20 11:02:27 +0200

    Use of 'Uint8Array' instead of 'Buffer'

--- libs/utils/buffer.utils.ts
+++ libs/utils/buffer.utils.ts
@@ -2,45 +2,46 @@
 import { TextUtils } from './text.utils';
 
 /**
-    * Utility class for operations related to Buffers.
+    * Utility class for operations related to Buffers (Uint8Array).
 */
 
 export class BufferUtils {
 
     /**
-        * Converts a Buffer to a string using the specified encoding.
+        * Converts a Buffer (Uint8Array) to a string using the specified encoding.
         *
-        * @param data - The Buffer to convert to a string.
-        * @param encoding - The encoding to use for conversion.
+        * @param data - The Buffer (Uint8Array) to convert to a string.
+        * @param encoding - The encoding to use for conversion (e.g., 'utf8', 'ascii').
         * @returns The string representation of the Buffer.
     */
 
     private static toString(
-        data: Buffer,
+        data: Uint8Array,
         encoding: BufferEncoding
     ): string {
-        return data.toString(encoding);
+        const decoder = new TextDecoder(encoding);
+        return decoder.decode(data);
     }
 
     /**
-        * Converts a Buffer to a UTF-8 encoded string.
+        * Converts a Buffer (Uint8Array) to a UTF-8 encoded string.
         *
-        * @param data - The Buffer to convert to a UTF-8 string.
+        * @param data - The Buffer (Uint8Array) to convert to a UTF-8 string.
         * @returns The UTF-8 string representation of the Buffer.
     */
 
-    public static toUTF8String(data: Buffer): string {
+    public static toUTF8String(data: Uint8Array): string {
         return this.toString(data, 'utf8');
     }
 
     /**
-        * Converts a Buffer to a trimmed UTF-8 encoded string.
+        * Converts a Buffer (Uint8Array) to a trimmed UTF-8 encoded string.
         *
-        * @param data - The Buffer to convert to a trimmed UTF-8 string.
+        * @param data - The Buffer (Uint8Array) to convert to a trimmed UTF-8 string.
         * @returns The trimmed UTF-8 string representation of the Buffer.
     */
 
-    public static toUTF8StringTrimmed(data: Buffer): string {
+    public static toUTF8StringTrimmed(data: Uint8Array): string {
         return TextUtils.stripWhitespace(this.toUTF8String(data));
     }
     
