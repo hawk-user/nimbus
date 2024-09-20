122f20442c86340772ca5aea2de80dfcb0520a13
Salem <salem.djekoun@gmail.com>
2024-09-20 11:06:37 +0200

    Make code more readable and understandable

--- libs/core/type.guard.ts
+++ libs/core/type.guard.ts
@@ -9,22 +9,22 @@ export class TypeGuard {
         * Safely casts a value to a specified type if a condition is met.
         *
         * @param value - The value to cast.
-        * @param condition - The condition to check before casting.
-        * @param errmsg - The error message to throw if the condition is not met.
+        * @param itsTheRightTypeOf - The condition to check before casting.
+        * This should be a function that takes a value and returns a boolean
+        * indicating if the value is of the desired type.
+        * @param wrongTypeError - The error message to throw if
+        * the condition is not met.
         * @returns The value cast to the specified type.
         * @throws Will throw an error if the condition is not met.
     */
-   
-    public static safeCast<T>(
+
+    public static safeCast<R>(
         value: unknown,
-        condition: () => boolean,
-        errmsg: string
-    ): T {
-        if (condition()) {
-            return value as T;
-        } else {
-            throw new Error(errmsg);
-        }
+        itsTheRightTypeOf: (val: unknown) => boolean,
+        wrongTypeError: string
+    ): R {
+        if (itsTheRightTypeOf(value)) return value as R;
+        else throw new Error(wrongTypeError);
     }
 
 }
