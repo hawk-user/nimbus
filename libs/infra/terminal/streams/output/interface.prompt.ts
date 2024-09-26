0bb31a2ccf37ded09daa3069cb0d977a8485cfbe
Salem <salem.djekoun@gmail.com>
2024-09-26 10:42:13 +0200

    Add Prompt interface

---
+++ libs/infra/terminal/streams/output/interface.prompt.ts
@@ -0,0 +1,14 @@
+
+/**
+    * Interface representing a prompt message.
+*/
+
+export interface Prompt {
+
+    /**
+        * The message to be displayed in the prompt.
+    */
+
+    message: string;
+    
+}
