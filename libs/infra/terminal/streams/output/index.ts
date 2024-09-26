b8547b8a3b7edb023d66b6d6cd27ad08e95a9f2c
Salem <salem.djekoun@gmail.com>
2024-09-26 10:46:50 +0200

    Export contents

---
+++ libs/infra/terminal/streams/output/index.ts
@@ -0,0 +1,5 @@
+export * from './interface.outgoing.port';
+export * from './interface.program.ouput';
+export * from './interface.prompt';
+export * from './outgoing.stream';
+export * from './output.formatter';
