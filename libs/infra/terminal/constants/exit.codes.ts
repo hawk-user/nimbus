d0e4c1b12f8b21c7ec98960ab02d42c5da99c346
Salem <salem.djekoun@gmail.com>
2024-09-26 10:19:43 +0200

    Add exit codes

---
+++ libs/infra/terminal/constants/exit.codes.ts
@@ -0,0 +1,18 @@
+
+/** 
+    * Indicates successful completion.
+*/
+
+export const DONE = 0;
+
+/** 
+    * Indicates that the maximum number of attempts has been reached.
+*/
+
+export const MAX_ATTEMPTS_REACHED = 254;
+
+/** 
+    * Indicates an unspecified error occurred.
+*/
+
+export const UNSPECIFIED_ERROR = 255;
