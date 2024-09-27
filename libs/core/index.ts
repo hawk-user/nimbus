60244adcdcfd5a23aa75c23bf679061f10c2f6b2
Salem <salem.djekoun@gmail.com>
2024-09-27 12:30:12 +0200

    Update directory name

--- libs/core/index.ts
+++ libs/core/index.ts
@@ -1,6 +1,6 @@
 export * from './events';
+export * from './guards'
 export * from './helpers'
 export * from './identifiers';
 export * from './operations';
 export * from './time';
-export * from './type'
