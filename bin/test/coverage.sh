63b3e24f5e2417fb4961110b67f5f05268270f7c
Salem <salem.djekoun@gmail.com>
2024-10-04 13:24:23 +0200

    Add a shell script to run the coverage test

---
+++ bin/test/coverage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Remove any existing coverage data to start fresh
+rimraf ./coverage
+
+# Run acceptance tests with Cucumber and collect coverage data
+nyc --reporter='json' --report-dir='./coverage/.tmp/acceptance-test' cucumber-js --config='./tests/setup/coverage/configat.yaml' > /dev/null
+
+# Run unit tests with Mocha and collect coverage data
+nyc --reporter='json' --report-dir='./coverage/.tmp/unit-test' mocha --config='./tests/setup/coverage/configut.yaml' > /dev/null
+
+# Merge the acceptance test coverage data into a combined coverage directory
+nyc merge './coverage/.tmp/acceptance-test' './coverage/.tmp/combined' > /dev/null
+
+# Merge the unit test coverage data into the combined coverage directory
+nyc merge './coverage/.tmp/unit-test' './coverage/.tmp/combined' > /dev/null
+
+# Generate the final coverage report in LCOV format
+nyc report --reporter='json' --report-dir='./coverage' --temp-directory='./coverage/.tmp'
+
+# # Clean up temporary coverage data
+rimraf ./coverage/.tmp
+
+# Notify the user where the coverage report is located
+echo "Coverage report generated in ./coverage"
